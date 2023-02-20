487f30fdb1ead7796b21ddeddb8ca1312c65d3ea
kart <mkartveli9@gmail.com>
2023-02-20 01:21:24 +0000

    5.1

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,11 +2,6 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-        int count = 0;
-        while (count != chars.length + 1){
-            System.out.println(chars);
-            count ++;
-        }
 
     }
 }
